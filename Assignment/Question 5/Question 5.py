4835a6e9c9da92c11d46dfeb78cea053e675eefe
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2020-11-25 18:15:23 +0530

    Create Question 5.py

---
+++ Assignment/Question 5/Question 5.py
@@ -0,0 +1,4 @@
+#Use a single print() statement to print the following string in the format shown. Twinkle, twinkle, little star,
+# How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are!
+
+print("Twinkle, twinkle, little star\nHow I wonder what you are! \nUp above the world so high,\nLike a diamond in the sky.\nTwinkle, twinkle, little star\nHow I wonder what you are!")
