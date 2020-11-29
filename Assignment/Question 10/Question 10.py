f608f83ad30af6360393ffaff413e940c37f53dd
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2020-11-29 12:17:53 +0530

    Update Question 10.py

--- Assignment/Question 10/Question 10.py
+++ Assignment/Question 10/Question 10.py
@@ -10,6 +10,10 @@
 #h. The program  will print A, then B, then it will alternate C’s and D’s ﬁve times and then ﬁnish with the letter E once.
 #i. If we wanted the above program to print ﬁve C’s followed by ﬁve D’s, instead of alternating C’s and D’s
 
+import random
+import string
+from pip._vendor.distlib.compat import raw_input
+
 print("-- Welcome -- \n-- choose any one -- \nA. Write a program that uses a for loop to print the numbers 8, 11, 14, 17, 20, . . . , 83, 86, 89\nb. Write a program that uses a for loop to print the numbers 100, 98, 96, . . . , 4, 2\nc. Write a program that uses exactly four for loops to print the sequence of letters below.AAAAAAAAAABBBBBBBCDCDCDCDEFFFFFFG\nd. A program gets 10 numbers from the user and counts how many of those numbers are greater than 10, how many are less then 10.\ne. print Hello a random number of times between 5 and 25.\nf. write a program that generates 10000 random numbers between 1 and 100 and counts how many of them are multiples of 12.")
 print("g. The program asks the user for a number and prints its square, then asks for another number and prints its square, etc. It does this three times and then prints that the loop is done.\nh. The program  will print A, then B, then it will alternate C’s and D’s ﬁve times and then ﬁnish with the letter E once.\ni. If we wanted the above program to print ﬁve C’s followed by ﬁve D’s, instead of alternating C’s and D’s")
 cho=input("\n-- Please Select Any Alphabet Number -- \n")
@@ -47,3 +51,31 @@ elif cho == "D" or cho == "d":
             down.append(lst[i])
     print("less then 10 :- ",up)
     print("greater than 10 :- ",down)
+elif cho == "E" or cho == "e":
+    n = random.randint(5, 25)
+    print(n)
+    for i in range(0,n):
+        print("hello")
+elif cho == "F" or cho == "f":
+    num = []
+    cou = 0;
+    for i in range(0,10000):
+        n = random.randint(1,100)
+        if (n % 12 == 0):
+            num.append(n)
+            cou += 1;
+    print("Total Number are multiples of 12 :- ", cou)
+    print(num)
+elif cho == "G" or cho == "g":
+    square = []
+    for i in range(0,3):
+        number = int (raw_input ("Enter an integer number: "))
+        square.append(number * number)
+    print ("Square is :- ",square)
+elif cho == "H" or cho == "h":
+    E = 5
+    print("Number of elements required : " + str(E))
+    res = ""
+    for idx in range(97, 97 + E):
+        res = res + chr(idx)
+    print("Alphabets till E are : " + str(res))
