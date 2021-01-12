d672f2006ae9e5bd978e799018cacef2e41a1ec5
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-12 14:49:27 +0530

    Delete Question 1.py

--- Assignment/Question 1/Question 1.py
+++
@@ -1,5 +0,0 @@
-#Create a Temperature Converter to perform ◦C = (5/9)×(◦F−32). Prompt user for temperature to convert and read the supplied value, perform the
-#conversion and report the result.
-deg = eval(input('Enter the temperature: '))
-temp = 5/9*(deg - 32);
-print("Convert velue", round(temp,2))
