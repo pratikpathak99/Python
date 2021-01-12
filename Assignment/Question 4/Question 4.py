437e81b559f35debe5c05833c2d22e39d748aeaa
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-12 14:50:02 +0530

    Delete Question 4.py

--- Assignment/Question 4/Question 4.py
+++
@@ -1,5 +0,0 @@
-#Ask a user to enter his name and then print following message. 'Hi user_name, Welcome to this course!'
-# Eg. Input: Sarvesh Output: Hi Sarvesh, Welcome to this course!
-
-name = input("Enter Your name: - ");
-print("Hi",name,", Welcome to this course!")
