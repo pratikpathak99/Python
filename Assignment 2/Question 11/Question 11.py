07e2d0db73f22584d6f6447fc7f8a5052e872608
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-16 14:20:58 +0530

    Create Question 11.py

---
+++ Assignment 2/Question 11/Question 11.py
@@ -0,0 +1,5 @@
+#11.	Create a list with different types of elements. Iterate over the list and print every element and type of element.
+
+data = [4, 'hello', 4, 'i am so happy', 'i like python', 11.99]
+for i in data:
+    print(i, ' = ', type(i), '\n')
