d9ea5f9a00b25168c166cb7a2fa775898c251377
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-16 14:20:31 +0530

    Update Question 3.py

--- Assignment 2/Question 3/Question 3.py
+++ Assignment 2/Question 3/Question 3.py
@@ -1,3 +1,6 @@
 # Write a program that generates a random number, x, between 1 and 50, a random number y between 2 and 5, and computes pow x y .
 
-
+import random
+x = random.randint(1,50)
+y = random.randint(2,5)
+print(x**y)
