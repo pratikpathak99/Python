b3d558de9769e36e1802752b83bd66e72ec83b46
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-16 14:20:46 +0530

    Create Question 16.py

---
+++ Assignment 2/Question 16/Question 16.py
@@ -0,0 +1,13 @@
+#16.	Write a demo program for list methods.
+
+H = [1, 2, 3, 4, 5, 6]
+L = [1, 2, 3, 4, 5, 6, 7, 8, 9, "pratik"]
+
+print('H =', H)
+H.pop(2)
+H.remove(5)
+del(H[2])  
+print('H =', H, '\n')
+
+
+
